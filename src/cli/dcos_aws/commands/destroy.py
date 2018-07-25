b7bea8194d52590d7353cb6a06d2a03a25f9de49
Adam Dangoor <adamdangoor@gmail.com>
2018-07-25 15:07:17 +0100

    Give regions to AWS CLI

--- src/cli/dcos_aws/commands/destroy.py
+++ src/cli/dcos_aws/commands/destroy.py
@@ -10,7 +10,8 @@ import click_spinner
 from cli.common.options import existing_cluster_id_option
 from cli.common.utils import check_cluster_id_exists
 
-from ._common import ClusterVMs, existing_cluster_ids
+from ._common import ClusterInstances, existing_cluster_ids
+from ._options import aws_region_option
 
 
 @click.command('destroy-list')
@@ -19,10 +20,12 @@ from ._common import ClusterVMs, existing_cluster_ids
     nargs=-1,
     type=str,
 )
+@aws_region_option
 @click.pass_context
 def destroy_list(
     ctx: click.core.Context,
     cluster_ids: List[str],
+    aws_region: str,
 ) -> None:
     """
     Destroy clusters.
@@ -30,7 +33,7 @@ def destroy_list(
     To destroy all clusters, run ``dcos-vagrant destroy $(dcos-vagrant list)``.
     """
     for cluster_id in cluster_ids:
-        if cluster_id not in existing_cluster_ids():
+        if cluster_id not in existing_cluster_ids(aws_region=aws_region):
             warning = 'Cluster "{cluster_id}" does not exist'.format(
                 cluster_id=cluster_id,
             )
@@ -40,20 +43,25 @@ def destroy_list(
         ctx.invoke(
             destroy,
             cluster_id=cluster_id,
+            aws_region=aws_region,
         )
 
 
 @click.command('destroy')
 @existing_cluster_id_option
-def destroy(cluster_id: str) -> None:
+@aws_region_option
+def destroy(cluster_id: str, aws_region: str) -> None:
     """
     Destroy a cluster.
     """
     check_cluster_id_exists(
         new_cluster_id=cluster_id,
-        existing_cluster_ids=existing_cluster_ids(),
+        existing_cluster_ids=existing_cluster_ids(aws_region=aws_region),
     )
-    cluster_vms = ClusterVMs(cluster_id=cluster_id)
+    cluster_instances = ClusterInstances(
+        cluster_id=cluster_id,
+        aws_region=aws_region,
+    )
     with click_spinner.spinner():
-        cluster_vms.destroy()
+        cluster_instances.destroy()
     click.echo(cluster_id)
