ef49a692964f098f7c989264481c6d71c645b90e
Adam Dangoor <adamdangoor@gmail.com>
2019-05-14 14:49:09 +0200

    Remove custom linter for init files

--- admin/custom_linters.py
+++ admin/custom_linters.py
@@ -109,23 +109,6 @@ def test_tests_collected_once() -> None:
     assert all_tests - tests_to_patterns.keys() == set()
 
 
-def test_init_files() -> None:
-    """
-    ``__init__`` files exist where they should do.
-
-    If ``__init__`` files are missing, linters may not run on all files that
-    they should run on.
-    """
-    directories = (Path('src'), Path('tests'))
-
-    for directory in directories:
-        files = directory.glob('**/*.py')
-        for python_file in files:
-            parent = python_file.parent
-            expected_init = parent / '__init__.py'
-            assert expected_init.exists()
-
-
 def test_pydocstyle() -> None:
     """
     Run ``pydocstyle`` and ignore errors.
